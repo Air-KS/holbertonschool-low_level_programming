94411d79ba28de3994c271ada3d00fa0490512c3
Air-KS <6265@holbertonstudents.com>
2023-02-16 06:57:37 -0800

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    char message[] = "\"Programming is like building a multilingual puzzle";
-    puts(message);
-    return 0;
+/*
+* main - Fonction principal avec void = Ne prend aucun argument
+*
+* Return: Always =  0
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
