970d8aa7b36e2bad6a9286a4d781a2433370e13c
Air-KS <6265@holbertonstudents.com>
2023-02-20 02:05:12 -0800

    1-last_digit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Fonction principal
+ * Description - Obtenir un nombre aléatoir, puis affichez-le
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 5)
+		printf("Last digit of %d is greater than 5\n", n);
+	if (n < 5)
+		printf("Last digit of %d is less than 6 and not 0\n", n);
+	if (n == 0)
+		printf("Last digit of %d is 0\n", n);
+	return (0);
+}
