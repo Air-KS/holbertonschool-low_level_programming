7178869f08c4352240f8c9aad101c57a56d63984
Air-KS <6265@holbertonstudents.com>
2023-02-20 06:48:29 -0800

    6-print_numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+	int cf;
+
+	for (cf ='0'; cf <= '9'; cf++)
+	{
+		putchar(cf);
+	}
+	putchar('\n');
+
+	return (0);
+}
+
