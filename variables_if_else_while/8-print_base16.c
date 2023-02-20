a6a494b37e8c938f929537cb292240e2d4bedade
Air-KS <6265@holbertonstudents.com>
2023-02-20 08:00:30 -0800

    8-print_base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Fonction principal
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alpha; int nombre;
+
+	for (nombre = '0'; nombre <= '9'; nombre++)
+	{
+		putchar(nombre);
+	}
+
+        for (alpha = 'a'; alpha <= 'f'; alpha++)
+        {
+		putchar(alpha);
+        }
+
+	putchar('\n');
+
+	return (0);
+
+
+}
