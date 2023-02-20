e103f233bbec304d4b7d90a09d02538d25807fa0
Air-KS <6265@holbertonstudents.com>
2023-02-20 05:52:27 -0800

    4-print_alphabt

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+ * main - Fonction principal
+ *
+ * Return: 0 
+ */
+
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			a++;
+		}
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
