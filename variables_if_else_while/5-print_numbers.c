9dbb97a897f95aab8c9bca57070bec7ea977b3f5
Air-KS <6265@holbertonstudents.com>
2023-02-20 06:11:15 -0800

    5-print_numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Fonction principal
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int cf = 0;
+
+	for (cf = '0'; cf <= '9'; cf++)
+	{
+		printf("%d\n", cf);
+	}
+
+	return (0);
+
+}
