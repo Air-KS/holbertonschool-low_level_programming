7b8ba4a4f8985a14404bcc955ea1a89bfce60e40
Air-KS <6265@holbertonstudents.com>
2023-02-20 08:40:33 -0800

    9-print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -Fonction principal
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+
+	{
+		putchar(number);
+
+		if (number != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return(0);
+}
