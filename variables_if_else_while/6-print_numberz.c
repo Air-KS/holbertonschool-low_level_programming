f2fa1dd661661683e485cfc0e5b3053a01242f14
Air-KS <6265@holbertonstudents.com>
2023-02-20 06:56:37 -0800

    6-print_numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/**
  * main - Fonction principal
  *
  * Return: 0
- *
  */
 
 int main(void)
 {
 	int cf;
 
-	for (cf ='0'; cf <= '9'; cf++)
+	for (cf = '0'; cf <= '9'; cf++)
 	{
 		putchar(cf);
 	}
