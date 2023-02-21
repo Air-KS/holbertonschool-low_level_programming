6a40f781966c61b934a8c639037cc30aa932c9af
Air-KS <6265@holbertonstudents.com>
2023-02-21 09:18:50 -0800

    7-print_last_digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: number
+ *
+ * Return: c
+ */
+
+int print_last_digit(int n)
+{
+	int c;
+
+	c = n % 10;
+
+	if (c < 0)
+	{
+		c = c * -1;
+	}
+	_putchar(c + '0');
+	return (c);
+}
