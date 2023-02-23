199c197be3ed69c4463ba8f6900f5ce508135f46
Air-KS <6265@holbertonstudents.com>
2023-02-23 05:53:44 -0800

    0-isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
 /**
+ *_isupper - Fonction
+ *@c:
  *
- *
+ * Return: 0 and 1
  */
 
 int _isupper(int c)
 {
-	if (c == 'a')
+	if (c >= 'A' && c <= 'Z')
 	{
-		return (0);
+		return (1);
+
 	}
-	return (1);
+	return (0);
 }
