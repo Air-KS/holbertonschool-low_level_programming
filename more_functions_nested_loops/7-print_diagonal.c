bda8fa51e2fb38411af63a089dec17db223e5ba8
Air-KS <6265@holbertonstudents.com>
2023-02-23 10:45:47 -0800

    7-print_diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print to diagonal
+ * @n: size
+ *
+ * Return: Always 0
+ */
+
+void print_diagonal(int n)
+{
+	int ligne, colonne;
+
+	if (n > 0)
+	{
+		for (ligne = 1; ligne < n; ligne++)
+		{
+			for (colonne = 1; ligne > colonne; colonne++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
