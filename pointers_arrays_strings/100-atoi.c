67e1843a7984de144c4d3d17d68c8e3900e46b0d
Air-KS <6265@holbertonstudents.com>
2023-02-28 09:53:46 -0800

    100-atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _atoi - function that convert a string to an integer.
+ * @s: pointer
+ *
+ * Return: num * signe
+ */
+
+int _atoi(char *s)
+{
+	int signe = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+		{
+			signe *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = (num * 10) + (*s - '0');
+		}
+		else if (num > 0)
+			break;
+	} while (s++);
+
+	return (num * signe);
+}
