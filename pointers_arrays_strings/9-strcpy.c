4efda375fccc5c51995f93d500cae46b4daea071
Air-KS <6265@holbertonstudents.com>
2023-02-28 08:13:15 -0800

    9-strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the string pointed to by src
+ * *@dest: pointer
+ * *@src: pointer
+ *
+ * Return: dest pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int index;
+
+	for (index = 0; src[index] != '\0'; index++)
+	{
+		dest[index] = src[index];
+	}
+	dest[index] = ('\0');
+
+	return (dest);
+}
