8022c705f6766f2bc02c401671bb758d0979a471
Air-KS <6265@holbertonstudents.com>
2023-03-02 06:34:47 -0800

    5-string_toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes uppercase
+ * @str: pointer
+ * Return: str
+ *
+ */
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+		{
+			str[index] = str[index] - 32;
+		}
+		index++;
+	}
+	return (str);
+}
