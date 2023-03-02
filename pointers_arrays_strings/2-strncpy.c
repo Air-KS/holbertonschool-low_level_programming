cd30d3a02c0e362da1758cb6e155cbf2d3b988dc
Air-KS <6265@holbertonstudents.com>
2023-03-02 01:18:36 -0800

    2-strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string.
+ * @dest:
+ * @src:
+ * @n:
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0;
+
+	for (index = 0; src[index] != '\0' && index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+
+	while (index < n)
+	{
+		dest[index] = '\0';
+		index++;
+	}
+	return (dest);
+}
