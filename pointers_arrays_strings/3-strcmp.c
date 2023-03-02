bca3e13939a40d901ad787adb6cc89e21bfd639a
Air-KS <6265@holbertonstudents.com>
2023-03-02 04:43:23 -0800

    3-strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings
+ * @s1: Value of pointer
+ * @s2: Value of pointer
+ * Return: *s1 - *s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
