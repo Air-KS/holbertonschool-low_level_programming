7f982639b6a37d2a31e410153f81f892ebac3058
Air-KS <6265@holbertonstudents.com>
2023-03-07 06:40:01 -0800

    prototype

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,3 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <unistd.h>
+
+
+/******** Function prototypes **********/
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -18,3 +29,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
