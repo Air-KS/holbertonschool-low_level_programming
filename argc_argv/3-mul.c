5a2b0c7f5da50034b6d43b5d5bbd13c9d47b5f6c
Air-KS <6265@holbertonstudents.com>
2023-03-08 06:04:32 -0800

    3-mul

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,17 +12,11 @@
 
 int main(int argc, char *argv[])
 {
-	int numberOne = atoi(argv[1]);
-	int numberTwo = atoi(argv[2]);
-
-	int resultat = numberOne * numberTwo;
-
 	if (argc != 3)
 	{
 		printf("Erreur\n");
 		return (1);
 	}
-	printf("%d\n", resultat);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
-
