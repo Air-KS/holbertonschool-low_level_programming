9f6cf9b068326217fcff3fb52688823b3182cbe0
Air-KS <6265@holbertonstudents.com>
2023-03-13 03:56:20 -0700

    0-create_array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 
 /**
-* *create_array - function that creates an array of chars, and initializes it with a specific char.
+* *create_array - function that creates an array of chars
 * @size: taille
 * @c: variable
-* 
+*
 * Return: array
 */
 
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    unsigned int index = 0;
+	char *array;
+	unsigned int index = 0;
 
-    if (size == 0)
-        return NULL;
+	if (size == 0)
+		return (NULL);
 
-    array = malloc(size * sizeof(char));
-    if (array == NULL)
-        return NULL;
+	array = malloc(size * sizeof(char));
+	if (array == NULL)
+		return (NULL);
 
-    for (index = 0; index < size; index++)
-        array[index] = c;
+	for (index = 0; index < size; index++)
+		array[index] = c;
 
-    return array;
+	return (array);
 }
