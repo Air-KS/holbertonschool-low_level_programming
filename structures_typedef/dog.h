a9c66b07ef6f17f5125f2d6a1d39e2c3cad6b2b4
Air-KS <kevinrogeret@gmail.com>
2023-03-20 10:08:38 +0000

    dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner dog
+ *
+ * Description: write description of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
