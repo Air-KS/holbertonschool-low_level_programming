3f3b0a64f94271a851bb4b5f43793a3d8543c283
Air-KS <kevinrogeret@gmail.com>
2023-03-20 13:40:20 +0000

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* free_dog - function that frees dogs
+* @d: pointer
+*
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name)
+		free(d->name);
+	
+	if (d->owner)
+		free(d->owner);
+	free(d);
+}
