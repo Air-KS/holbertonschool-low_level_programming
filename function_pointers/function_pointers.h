1f1783308d8ea85a7653bd1473e69dc780d85af8
Air-KS <kevinrogeret@gmail.com>
2023-03-21 13:14:42 +0000

    Prototype

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
 /******** Bibliothèque prototypes **********/
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,3 +12,5 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
