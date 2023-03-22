781033ab2c57321061838819f81057b356a9d9df
Air-KS <kevinrogeret@gmail.com>
2023-03-22 10:24:42 +0000

    Function that returns the sum of all its parameters

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,8 +10,10 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
+	int sum = 0; /* Initialise la somme à 0 */
 	unsigned int index;
+
+	/* Initialise la Variable args */
 	va_list arguments;
 
 	if (n == 0)
@@ -19,12 +21,15 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	}
 
+	/* Initialise la Variable args */
 	va_start(arguments, n);
+	/* Boucle à travers chaque argument variable */
 	for (index = 0; index < n; index++)
 	{
+		/* Ajoute l'argument courant à sum */
 		sum += va_arg(arguments, int);
 	}
-	va_end(arguments);
+	va_end(arguments); /* Termine la list d'argument variable */
 
 	return (sum);
 }
