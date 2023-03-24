81d44c996916fc1f2417b379e7ce2630b63f2eb0
Air-KS <kevinrogeret@gmail.com>
2023-03-24 17:37:27 +0000

    Function that prints anything

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -32,6 +32,7 @@ void print_all(const char * const format, ...)
 				if (string == NULL)
 				{
 					printf("(nil)"); /* La chaine est Null Affiche (nil)*/
+					continue;
 				}
 				printf("%s", string); /* Sinon, affiche la chaines */
 				break;
