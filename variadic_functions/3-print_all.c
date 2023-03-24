69837aa768fdf5e5844eb363b93d175073107859
Air-KS <kevinrogeret@gmail.com>
2023-03-24 14:36:13 +0000

    Function that prints anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+
+/**
+* print_all - Function that prints anything
+* @format: Paramettre
+*
+*/
+
+void print_all(const char * const format, ...)
+{
+	int index = 0;
+	char *string;
+	va_list argument;
+
+	va_start(argument, format);
+
+	while (format && format[index])
+	{
+		switch (format[index])
+		{
+			case 'c':
+			printf("%c", va_arg(argument, int));
+			break;
+
+			case 'i':
+			printf("%d", va_arg(argument, int));
+			break;
+
+			case 'f':
+			printf("%f", va_arg(argument, double));
+			break;
+
+			case 's':
+			string = va_arg(argument, char *);
+			if (string == NULL)
+				printf("(nil)"); /* Si la chaine est Null Affiche (nil)*/
+			else
+				printf("%s", string); /* Sinon, affiche la chaines */
+			break;
+
+		default:
+			index++;
+			continue; /* Ignore tout autre caractère */
+		}
+		if (format[index + 1] != '\0')
+		printf(", ");
+		index++;
+	}
+	va_end(argument);
+	printf("\n");
+}
