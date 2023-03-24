42a6fc1bd28b259b356a589df4a0d89a001d51fb
Air-KS <kevinrogeret@gmail.com>
2023-03-24 17:12:07 +0000

    Function that prints strings

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
 * print_strings - Function that prints strings
@@ -37,3 +39,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	va_end(arguments); /* Termine la liste d'argument */
 }
+
+
