4b31dfcbb0fc992eb8d76bb79b4d88d587537750
Air-KS <kevinrogeret@gmail.com>
2023-04-03 14:03:40 +0000

    Function that adds a new node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,26 +16,35 @@ int get_length (const char *str)
 	}
 	return (length);
 }
-
 /**
-*
-*
-*
-*
+* add_node - function that adds a new node
+* @head: pointer to pointer
+* @str: takes in a string
+* Return: pointer to the head
 */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	char *test_free;
 
 	if (head == NULL)
 		return (0);
 	
+
 	new_node = malloc (sizeof(list_t));
 
 	if (new_node == NULL)
 		return (0);
 
-	new_node->str = strdup(str);
+	test_free = strdup(str);
+
+	if (test_free == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+
+	new_node->str = test_free;
 	new_node->len = get_length(str);
 	new_node->next = *head;
 	*head = new_node;
