ba6a2fd967011fd98f58caabd86fdfd0fa3996f4
Air-KS <kevinrogeret@gmail.com>
2023-04-07 07:24:13 +0000

    Create just file

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+* delete_dnodeint_at_index -  function that deletes the node at index
+* @head: double pointer
+* @index: index
+*
+*/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+[
+
+]
