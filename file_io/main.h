bf52b22f5b42ddedd35c12143ca47aec25ddfd6d
Air-KS <kevinrogeret@gmail.com>
2023-04-10 13:13:39 +0000

    Prototype

---
+++ file_io/main.h
@@ -0,0 +1,18 @@
+#ifndef PROTOTYPE_H
+#define PROTOTYPE_H
+
+/******** Bibliothèque prototypes **********/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/******** Struct _printf **********/
+
+
+/******** Function prototypes **********/
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
